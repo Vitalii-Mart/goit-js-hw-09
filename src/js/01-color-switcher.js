d4df2e79cdee718c5228303fc2a8c399006998be
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-03-02 22:20:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,27 @@
+const startEl = document.querySelector('[data-start]');
+const stopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
-const startEl = document.querySelector()
+let timerId = null;
 
+stopEl.disabled = true;
 
+startEl.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  setTimeout(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 0);
+  startEl.disabled = true;
+  stopEl.disabled = false;
+});
+stopEl.addEventListener('click', () => {
+  clearInterval(timerId);
 
-
-
-
-
-
+  startEl.disabled = false;
+  stopEl.disabled = true;
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
